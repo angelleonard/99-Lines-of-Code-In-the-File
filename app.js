904101fd5fddd8b25ec5d4ad5291c556ed12c8f0
angelleonard <aleonard0215@aol.com>
2019-01-15 17:38:36 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+let friends = ['Matt', 'Eliana', 'Noelle', 'Clare', 'Katie'];
+
+for (let i = 0; i < friends.length; i++) {
+       //each friend is friends[i]
+       for (let j = 99; j > 0; j--) {
+              if (j > 1){	
+              console.log( j + " lines of code in the file, " + j + " lines of code. " + friends[i] + " strikes one out, clears it all out, " + [j - 1] + " lines of code in the file");
+              } else {
+                console.log ( j + " line of code in the file, " + j + " line of code. " + friends[i] + " strikes one out, clears it all out, no more lines of code in the file.")
+                break;
+              }
+         
+           
+       
+       }
+}
